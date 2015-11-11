966e4924b6e3d5faab2dc6e3d9002772d96622a6
Dan Reeves <heydanreeves@gmail.com>
2015-11-11 17:51:43 +0000

    woop thats nice

--- Assets/Companion.cs
+++ Assets/Companion.cs
@@ -1,17 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof (NavMeshAgent))]
-[RequireComponent(typeof (ThirdPersonCharacter))]
+[RequireComponent(typeof(Animator))]
+
 public class Companion : MonoBehaviour {
+
+    protected AvatarIKGoal ownHand;
+    protected Animator animator;
+    protected NavMeshAgent navAgent;
+    private bool jDown = false;
+    public Transform heroHand;
 
 	// Use this for initialization
 	void Start () {
-
+        ownHand = AvatarIKGoal.LeftHand;
+        animator = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void OnAnimatorIK () {
+
+        if (Input.GetKeyDown("j")) {
+            jDown = true;
+        }
+        if (Input.GetKeyUp("j")) {
+            jDown = false;
+        }
+
+        float distance = Vector3.Distance(animator.GetIKPosition(ownHand), heroHand.position);
+        Debug.Log(distance);
+        if (distance < 1.5f & jDown) {
+            navAgent.stoppingDistance = 0.0f;
+            navAgent.speed = 1.2f;
+        } else {
+            navAgent.stoppingDistance = 999.0f;
+            navAgent.speed = 0.8f;
+        }
 
 	}
 }
---
+++ Assets/IKScriptCompanion.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class IKScriptCompanion : MonoBehaviour {
+
+    protected AvatarIKGoal hand;
+
+    public bool ikActive = false;
+    public Transform rightHandObj = null;
+    public Transform lookObj = null;
+    public bool rightHand = true;
+
+
+    protected AvatarIKGoal ownHand;
+    protected Animator animator;
+    protected NavMeshAgent navAgent;
+    public Transform heroHand;
+
+
+    void Start ()
+    {
+
+        ownHand = AvatarIKGoal.LeftHand;
+        animator = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
+
+        animator = GetComponent<Animator>();
+        if (rightHand) {
+            hand = AvatarIKGoal.RightHand;
+        } else {
+            hand = AvatarIKGoal.LeftHand;
+        }
+    }
+
+    //a callback for calculating IK
+    void OnAnimatorIK()
+    {
+
+        if (Input.GetKeyDown("j")) {
+            ikActive = true;
+        }
+        if (Input.GetKeyUp("j")) {
+            ikActive = false;
+        }
+
+
+        if(animator) {
+
+            float distance = Vector3.Distance(animator.GetIKPosition(ownHand), heroHand.position);
+
+            //if the IK is active, set the position and rotation directly to the goal.
+            if(ikActive) {
+
+                // Set the look target position, if one has been assigned
+                if(lookObj != null) {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(lookObj.position);
+                }
+
+                // Set the right hand target position and rotation, if one has been assigned
+                if(rightHandObj != null) {
+                    animator.SetIKPositionWeight(hand, 1);
+                    animator.SetIKRotationWeight(hand, 1);
+                    animator.SetIKPosition(hand, rightHandObj.position);
+                    animator.SetIKRotation(hand, rightHandObj.rotation);
+                }
+
+            }
+
+            //if the IK is not active, set the position and rotation of the hand and head back to the original position
+            else {
+                animator.SetIKPositionWeight(hand, 0);
+                animator.SetIKRotationWeight(hand, 0);
+                animator.SetLookAtWeight(0);
+            }
+
+            Debug.Log(distance);
+            if (distance < 2.0f & ikActive) {
+                navAgent.stoppingDistance = 0.8f;
+                navAgent.speed = 1.2f;
+            } else {
+                navAgent.stoppingDistance = 999.0f;
+                navAgent.speed = 0.8f;
+            }
+
+
+        }
+    }
+}
